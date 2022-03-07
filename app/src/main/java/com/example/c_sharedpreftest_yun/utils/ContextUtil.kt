0a640e031b1d9b19aca3f6bcab1df539a7c3de34
Roy <J.jyun0525@gmail.com>
2022-03-07 12:15:19 +0900

    저장된 자동 로그인 여부 확인기능 추가

---
+++ app/src/main/java/com/example/c_sharedpreftest_yun/utils/ContextUtil.kt
@@ -0,0 +1,27 @@
+package com.example.c_sharedpreftest_yun.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object {
+
+        private val prefName = "TestPref"
+
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+        fun setAutoLogin(context: Context, autoLogin: Boolean) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+        }
+
+        fun getAutoLogin(context: Context) : Boolean {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(AUTO_LOGIN, false)
+
+        }
+
+    }
+}
